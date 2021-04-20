f85200ba8283b18c3546421c04008657d26836cf
Hazem Ben Hassine <hazem.benhassine@vynd.com>
2021-04-20 05:20:50 +0100

    This

--- src/app/pages/premium-content/chat-list/chat-list.component.ts
+++ src/app/pages/premium-content/chat-list/chat-list.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatListComponent implements OnInit {
 
+  isRelationExpanded: boolean;
+  isRelationDisplayed: boolean;
+  isChatExpanded: boolean;
+  isChatDisplayed: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleChat(): void {
+    this.isChatDisplayed = true;
+    this.isRelationDisplayed = false;
+  }
+
+  toggleRelation(): void {
+    this.isChatDisplayed = false;
+    this.isRelationDisplayed = true;
   }
 
 }
